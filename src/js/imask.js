bbbc467ffcc3a5bcff6e79177d92b21fa2ebf670
Alexandra789 <zakovinko99@gmail.com>
2023-10-28 14:48:23 +0300

    fix formatted

--- src/js/imask.js
+++ src/js/imask.js
@@ -1,6 +1,6 @@
-import IMask from "imask";
+import IMask from 'imask';
 
-const phoneInput = document.querySelector("#inputPhone");
+const phoneInput = document.querySelector('#inputPhone');
 
 const phoneMask = new IMask(phoneInput, {
 	mask: "+{000}(00)000-00-00",
