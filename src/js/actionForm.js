7f55cf9f53e39b794ffbf7ea8cb3fd88ca666856
Alexandra789 <zakovinko99@gmail.com>
2023-10-29 16:23:35 +0300

    implement reset form function

--- src/js/actionForm.js
+++ src/js/actionForm.js
@@ -1,17 +1,38 @@
 import {API} from './api';
 import {createNotification} from "./notification";
 
+const body = document.querySelector('body');
 const form = document.querySelector('#form');
+const labels = document.querySelectorAll('.form__label');
+
 export const sendForm = async () => {
-	form.classList.add('sending');
+	addSendingForm();
 	const response = await API();
 	if (response.ok) {
 		const result = await response.json();
-		form.classList.remove('sending');
+		removeSendingForm();
+		resetForm();
 		createNotification(result);
 	} else {
 		const result = await response.json();
-		form.classList.remove('sending');
+		removeSendingForm();
 		createNotification(result);
 	}
+}
+
+const resetForm = () => {
+	labels.forEach(label => {
+		form.reset();
+		label.classList.remove('top');
+	})
+}
+
+const addSendingForm = () => {
+	form.classList.add('sending');
+	body.classList.add('hidden');
+}
+
+const removeSendingForm = () => {
+	form.classList.remove('sending');
+	body.classList.remove('hidden');
 }
