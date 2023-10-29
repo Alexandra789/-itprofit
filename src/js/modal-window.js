43a4aed0d270e1a99b06802837fb7a2a7344c550
Alexandra789 <zakovinko99@gmail.com>
2023-10-29 16:22:32 +0300

    hidden scroll when modal window open

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,9 +1,11 @@
 const modal = document.querySelector('.modal');
 const openBtn = document.querySelector('.modal-window-btn');
 const closeBtn = document.querySelector('.modal__close-button');
+const body = document.querySelector('body');
 
 function toggleModal() {
 	modal.classList.toggle('show-modal');
+	body.classList.toggle('hidden');
 }
 
 function windowOnClick(event) {
