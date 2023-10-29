d5eb0db246f589b0103b8aed8d628b324316c0d7
Alexandra789 <zakovinko99@gmail.com>
2023-10-29 16:22:07 +0300

    fix phone mask

--- src/js/imask.js
+++ src/js/imask.js
@@ -3,5 +3,5 @@ import IMask from 'imask';
 const phoneInput = document.querySelector('#inputPhone');
 
 const phoneMask = new IMask(phoneInput, {
-	mask: "+{000}(00)000-00-00",
+	mask: "+{000} (00) 000-00-00",
 });
